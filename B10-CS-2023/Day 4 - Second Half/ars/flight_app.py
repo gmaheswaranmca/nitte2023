3b42435dfb67c194deabd33356b44df3091fdc39
gmaheswaranmca <gmaheswaranmca@gmail.com>
2023-08-08 16:04:38 +0530

    "notes"

--- B10-CS-2023/Day 4 - Second Half/ars/flight_app.py
+++ B10-CS-2023/Day 4 - Second Half/ars/flight_app.py
@@ -31,8 +31,68 @@ class FlightManagement:
         cur.execute(sql, params)
         con.commit()
         print("Flight is added successfully.")
+    def UpdatePrice(self):
+        flight_number = input("Enter flight number:")
+        con = self.DbConnect()
+        cur = con.cursor()
+        sql = """SELECT * FROM flight WHERE flight_number=%s"""
+        params = (flight_number,)
+        cur.execute(sql, params)
+        flight = cur.fetchone()
 
+        print(f"""Flight Number:{flight[1]}
+        Airline Name: {flight[2]}
+        Source-Destination: {flight[3]}-{flight[4]}
+        Ticket Fare: {flight[7]}
+        """)
+
+        new_ticket_fare = float(input("Enter new ticket fare:"))
+        sql = "UPDATE flight SET ticket_fare=%s WHERE flight_number=%s"
+        params = (new_ticket_fare, flight_number)
+        cur.execute(sql, params)
+        con.commit()
+        print("Price has updated successfully")
+        con.close()
+
+    def DeleteByNumber(self):
+        flight_number = input("Enter flight number:")
+        con = self.DbConnect()
+        cur = con.cursor()
+        sql = """SELECT * FROM flight WHERE flight_number=%s"""
+        params = (flight_number,)
+        cur.execute(sql, params)
+        flight = cur.fetchone()
+
+        print(f"""Flight Number:{flight[1]}
+        Airline Name: {flight[2]}
+        Source-Destination: {flight[3]}-{flight[4]}
+        Ticket Fare: {flight[7]}
+        """)
+
+        confirm = input("Are you sure to delete(y/n)?")
+        if(confirm == "y"):
+            sql = "DELETE FROM flight WHERE flight_number=%s"
+            params = (flight_number,)
+            cur.execute(sql, params)
+            con.commit()
+            print("Flight has deleted successfully")
+        con.close()
+    def ListAllFlights(self):
+        con = self.DbConnect()
+        cur = con.cursor()
+        sql = """SELECT * FROM flight"""
+        params = ()
+        cur.execute(sql, params)
+        flights = cur.fetchmany()
+        print("Flight#(Airline Name)\tSource-Destination\tTravel Date\tSeats\tFare")
+        #print(flights)
+        for e in flights:
+            print(f"""{e[1]}({e[2]})\t{e[3]}-{e[4]}\t{e[5]}\t{e[6]}\t{e[7]}""")
+        con.close()
 
 app = FlightManagement()
-app.AddFlight()
+#app.AddFlight()
+#app.UpdatePrice()
+#app.DeleteByNumber()
+app.ListAllFlights()
         
